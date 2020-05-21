71b51e49621c4fed942924e6fdb1d850265c1a9d
kyle-law <junwei_1121@hotmail.com>
2020-05-21 21:32:18 +0800

    Add active upon scrolling to section

--- js/app.js
+++ js/app.js
@@ -53,3 +53,37 @@
 // Scroll to section on link click
 
 // Set sections as active
+function debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function() {
+      const context = this, args = arguments;
+      const later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      const callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderSections = document.querySelectorAll('section')
+
+function checkSlide() {
+    sliderSections.forEach(sliderSection => {
+      // half way through the section
+      const slideInAt = (window.scrollY + window.innerHeight) - sliderSection.offsetHeight / 2;
+      // bottom of the section
+      const sectionBottom = sliderSection.offsetTop + sliderSection.offsetHeight;
+      const isHalfShown = slideInAt > sliderSection.offsetTop;
+      const isNotScrolledPast = window.scrollY < sectionBottom -200;
+      if (isHalfShown && isNotScrolledPast) {
+        sliderSection.classList.add('your-active-class');
+      } else {
+        sliderSection.classList.remove('your-active-class');
+      }
+    });
+  }
+
+window.addEventListener('scroll',debounce(checkSlide));
